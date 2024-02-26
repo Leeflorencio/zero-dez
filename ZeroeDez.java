55626811878e7c56187ab9369dd5a845689c1853
Leticia Florêncio <70420362+Leeflorencio@users.noreply.github.com>
2024-02-25 21:28:52 -0300

    Update ZeroeDez.java
    Exclusão de comentarios

--- ZeroeDez.java
+++ ZeroeDez.java
@@ -6,14 +6,7 @@ public class ZeroeDez {
 
     public static void main(String[] args) {
 
-        /*
-            Faça um programa que peça uma nota, entre zero e dez.
-            Mostre uma mensagem caso o valor seja inválido e continue pedindo
-            até que o usuário informe um valor válido
-         */
-
         Scanner sc = new Scanner(System.in);
-
 
         do{
             System.out.println("Informe uma nota entre 0 e 10: ");
